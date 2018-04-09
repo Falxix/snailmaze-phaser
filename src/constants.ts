751689ffce87c14af23c54853b8a2663caa7bc76
Mike Wilkinson <mwilkinson@imo-online.com>
2018-04-08 20:11:46 -0500

    Revert the constants change

--- src/constants.ts
+++ src/constants.ts
@@ -32,7 +32,7 @@ export class Constants{
     
     public static readonly StartSpriteName: string = 'snailStart';
     public static readonly EndSpriteName: string = 'goal';
-    public static readonly StartTime : number = 10;
+    public static readonly StartTime : number = 60;
     public static readonly WelcomePosition: Phaser.Point=new Phaser.Point(100,100);
     public static readonly RoundPosition: Phaser.Point=new Phaser.Point(400,150);
     public static readonly TimePosition: Phaser.Point=new Phaser.Point(500,150);    
